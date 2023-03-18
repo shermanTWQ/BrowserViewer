f8108671fede183fc2842e93517508e2975ecdc7
neonxflame <124432647+shermanTWQ@users.noreply.github.com>
2023-03-18 12:38:00 +0800

    Add files via upload

---
+++ DBViewer/BrowserViewer.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BrowserViewer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtURL = New System.Windows.Forms.TextBox()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.btnHelp = New System.Windows.Forms.Button()
+        Me.WBMain = New System.Windows.Forms.WebBrowser()
+        Me.SuspendLayout()
+        '
+        'txtURL
+        '
+        Me.txtURL.Location = New System.Drawing.Point(12, 12)
+        Me.txtURL.Name = "txtURL"
+        Me.txtURL.Size = New System.Drawing.Size(845, 22)
+        Me.txtURL.TabIndex = 0
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(863, 11)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(62, 23)
+        Me.btnGo.TabIndex = 1
+        Me.btnGo.Text = "Go!"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'btnHelp
+        '
+        Me.btnHelp.Location = New System.Drawing.Point(932, 11)
+        Me.btnHelp.Name = "btnHelp"
+        Me.btnHelp.Size = New System.Drawing.Size(62, 23)
+        Me.btnHelp.TabIndex = 2
+        Me.btnHelp.Text = "Help"
+        Me.btnHelp.UseVisualStyleBackColor = True
+        '
+        'WBMain
+        '
+        Me.WBMain.AllowWebBrowserDrop = False
+        Me.WBMain.IsWebBrowserContextMenuEnabled = False
+        Me.WBMain.Location = New System.Drawing.Point(12, 49)
+        Me.WBMain.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WBMain.Name = "WBMain"
+        Me.WBMain.ScriptErrorsSuppressed = True
+        Me.WBMain.Size = New System.Drawing.Size(982, 660)
+        Me.WBMain.TabIndex = 3
+        Me.WBMain.TabStop = False
+        Me.WBMain.Url = New System.Uri("https://www.google.com", System.UriKind.Absolute)
+        Me.WBMain.WebBrowserShortcutsEnabled = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1006, 721)
+        Me.Controls.Add(Me.WBMain)
+        Me.Controls.Add(Me.btnHelp)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.txtURL)
+        Me.Name = "Form1"
+        Me.Text = "SecurU Browser 1.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtURL As TextBox
+    Friend WithEvents btnGo As Button
+    Friend WithEvents btnHelp As Button
+    Friend WithEvents WBMain As WebBrowser
+End Class
---
+++ DBViewer/BrowserViewer.vb
@@ -0,0 +1,27 @@
+﻿Public Class BrowserViewer
+
+    'Browser Viewer 1.0
+    'Provided for non-commercial use. Please refer to Github Repo for more information. 
+    'Contact: Sherman_tan@outlook.com
+    '(C) Tan Wei Qiang Sherman. 
+
+    Private Sub btnHelp_Click(sender As Object, e As EventArgs) Handles btnHelp.Click
+        MsgBox("Powered by Internet Explorer 11. Compatible View with Internet Explorer 7.")
+    End Sub
+
+    Private Sub btnGo_Click(sender As Object, e As EventArgs) Handles btnGo.Click
+        UseWaitCursor = True
+        WBMain.Navigate(txtURL.Text)
+        UseWaitCursor = False
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtURL.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+    End Sub
+End Class
